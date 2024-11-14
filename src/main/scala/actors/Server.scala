08b5afbf0fc0579e017d63cc2edc34d6488ba3eb
Dzenali <dzenetan.aliti@gmail.com>
2024-11-14 16:21:33 +0100

    query Slick fonctionnel

--- src/main/scala/actors/Server.scala
+++ src/main/scala/actors/Server.scala
@@ -6,8 +6,44 @@ import com.typesafe.config._
 import slick.jdbc.JdbcBackend.Database
 import scala.concurrent.ExecutionContext.Implicits.global
 import slick.jdbc.MySQLProfile.api._
+import scala.concurrent.Await
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration
+import scala.concurrent.{ Await, Future }
 
 
+class Team(tag: Tag) extends Table[(Int, String)](tag, "TEAM") {
+  def teamid = column[Int]("TeamId", O.PrimaryKey)
+  def teamname = column[String]("TeamName")
+  def * = (teamid, teamname)
+}
+
+val teams = TableQuery[Team]
+
+
+class Client(tag: Tag) extends Table[(Int, String, Int)](tag, "CLIENT") {
+  def clientid = column[Int]("ClientId", O.PrimaryKey)
+  def clientname = column[String]("ClientName")
+  def teamkey = column[Int]("TeamKey")
+  def * = (clientid, clientname, teamkey)
+  def team = foreignKey("TeamFK", teamkey, teams)(_.teamid)
+}
+
+val clients = TableQuery[Client]
+
+
+class User(tag: Tag) extends Table[(Int, String, Int, Int)](tag, "USER") {
+  def userid = column[Int]("UserId", O.PrimaryKey)
+  def username = column[String]("UserName")
+  def clientkey = column[Int]("ClientKey")
+  def userkey = column[Int]("UserKey")
+  def * = (userid, username, clientkey, userkey)
+  def team = foreignKey("ClientKey", clientkey, clients)
+  def user = foreignKey("UserKey", userkey, clients)
+}
+
+val user = TableQuery[User]
 
 object DatabaseConnectivity extends App {
   //JDBC url for connectivity
@@ -37,42 +73,17 @@ object DatabaseConnectivity extends App {
   }  
 
   val db = Database.forConfig("mydb")
+  
+  val q = teams.map(_.teamname)
+  val action = q.result
+  val result = db.run(action)
+  val sql = action.statements
+  Await.ready(result, Duration.Inf)
 
+  print(result)
 }
 
 object Server {
   sealed trait ServerCommand
   final case class getSmtg()
 }
-
-
-
-class Team(tag: Tag) extends Table[(Int, String)](tag, "team") {
-  def teamid = column[Int]("TeamId", O.PrimaryKey)
-  def teamname = column[String]("TeamName")
-  def * = (teamid, teamname)
-}
-
-val teams = TableQuery[Team]
-
-class Client(tag: Tag) extends Table[(Int, String, Int)](tag, "client") {
-  def clientid = column[Int]("ClientId", O.PrimaryKey)
-  def clientname = column[String]("ClientName")
-  def teamkey = column[Int]("TeamKey")
-  def * = (clientid, clientname, teamkey)
-  def team = foreignKey("TeamFK", teamkey, teams)(_.teamid)
-}
-
-val clients = TableQuery[Client]
-
-class User(tag: Tag) extends Table[(Int, String, Int, Int)](tag, "user") {
-  def userid = column[Int]("UserId", O.PrimaryKey)
-  def username = column[String]("UserName")
-  def clientkey = column[Int]("ClientKey")
-  def userkey = column[Int]("UserKey")
-  def * = (userid, username, clientkey, userkey)
-  def team = foreignKey("ClientKey", clientkey, clients)
-  def user = foreignKey("UserKey", userkey, clients)
-}
-
-val user = TableQuery[User]
