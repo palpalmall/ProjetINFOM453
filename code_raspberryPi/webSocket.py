ef8fc2e33370f54b3ded60205dfea17055749ba3
palpalmall <maelquestiaux@gmail.com>
2024-11-19 15:35:26 +0100

    test websocket

--- code_raspberryPi/webSocket.py
+++ code_raspberryPi/webSocket.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from websockets.sync.client import connect
+import json
 
 def hello():
     with connect("ws://localhost:8765") as websocket:
@@ -8,4 +9,19 @@ def hello():
         message = websocket.recv()
         print(f"Received: {message}")
 
-hello()
+#hello()
+
+my_websocket = connect("ws://localhost:8765")
+ping = {
+    "id" : 1015,
+    "nom" : "Marlene",
+    "id_sender" : 1010
+}
+
+def sendPing(websocket, data_struct) :
+    json_data_struct = json.dumps(data_struct)
+    websocket.send(json_data_struct)
+    message = websocket.recv()
+    print(f"Received: {message}")
+
+sendPing(my_websocket, ping)
