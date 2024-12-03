2bd8f971123947560670c1e55706ed71478dac39
Dzenali <dzenetan.aliti@gmail.com>
2024-12-03 13:55:47 +0100

    server Scalatra w modif on TeamManagerActor Create Team to return a JSON ok format

--- src/main/scala/Actor.scala
+++ src/main/scala/Actor.scala
@@ -1,4 +1,4 @@
-package com.example.app
+package actors
 
 import akka.actor.typed.Behavior
 import akka.actor.typed.PostStop
@@ -8,11 +8,33 @@ import akka.actor.typed.scaladsl.ActorContext
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.ActorRef
 import akka.actor.typed.ActorSystem
-import akka.actor.Actor
+import scala.util.{Try, Success, Failure}
+/* import akka.actor.Actor
 
 //a toy actor to test our scalatra servlet
 class Mactor extends Actor {
   def receive: Receive = {
     case "One" => sender() ! List("two")
   }
+}
+
+class Transaction extends Actor {
+  def receive: Receive = {
+    case _ => Behaviors.same
+  }
+} */
+
+object TransactionT {
+  def apply(id: String): Behavior[TransactionMessage] = Behaviors.setup { context =>
+    
+
+    Behaviors.receiveMessage {
+      case Tell(msg) =>
+        val tellmsg = Try {
+          
+        }
+        Behaviors.same
+      }
+      
+    }
 }
--- src/main/scala/ScalatraBootstrap.scala
+++ src/main/scala/ScalatraBootstrap.scala
@@ -1,18 +1,24 @@
-import com.example.app.*
+import actors.*
+
 import org.scalatra.*
 import jakarta.servlet.ServletContext
 import _root_.akka.actor.{ActorRef, ActorSystem, Props}
+import akka.actor.typed.ActorSystem as TAS
+import akka.actor.typed.SpawnProtocol
 
 
 //initializer for scalatra servlet
 class ScalatraBootstrap extends LifeCycle {
     // here is the init of our first actor. Might need to init all if we can't dynamically create child. We should be able to but require a good mapping.
-    val system = ActorSystem()
-    val myActor: ActorRef = system.actorOf(Props[Mactor](), "myActor")
-
+    //val system = ActorSystem()
+    //val myActor: ActorRef = system.actorOf(Props[Mactor](), "myActor")
+    val system :TAS[TeamManagerCommand] = TAS(TeamManagerActor(), "TeamManagerActor")
 
     override def init(context: ServletContext): Unit = {
-        context.mount(new PopServer(system, myActor), "/*")
+        //context.mount(new PopServer(system, myActor), "/*")
+        //context.setInitParameter(org.scalatra.EnvironmentKey, "production")
+        //context.setInitParameter(ScalatraBase.HostNameKey, "popsapp")
+        context.mount(new PopServerTyped(system), "/*")
         context.setInitParameter(org.scalatra.EnvironmentKey, "production")
         context.setInitParameter(ScalatraBase.HostNameKey, "popsapp")
     }
